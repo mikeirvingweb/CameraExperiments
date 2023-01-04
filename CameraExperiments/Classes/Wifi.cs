b9649e2d6347d060ba1716c77f9d24f585a7b05e
Mike Irving <c-mirving@kahua.com>
2023-01-04 17:41:18 +0000

    feat: support wifi on linux (experimentsl)

--- CameraExperiments/Classes/Wifi.cs
+++ CameraExperiments/Classes/Wifi.cs
@@ -1,5 +1,7 @@
 ﻿using SimpleWifi;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 
 namespace CameraExperiments
@@ -10,15 +12,51 @@ namespace CameraExperiments
 
         public bool WifiAvailable()
         {
-            return !wifi.NoWifiAvailable;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return true;
+            }
+            else
+            {
+                return !wifi.NoWifiAvailable;
+            }
         }
 
         public void WifiDisconnect()
         {
             Console.WriteLine("Disconnecting from current Wifi Network.");
 
-            if (wifi.ConnectionStatus == WifiStatus.Connected)
-                wifi.Disconnect();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo()
+                    {
+                        FileName = "nmcli",
+                        Arguments = "d disconnect wlan0",
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    };
+
+                    Process proc = new Process() { StartInfo = startInfo };
+                    proc.Start();
+
+                    string output = proc.StandardOutput.ReadToEnd();
+                    proc.WaitForExit();
+
+                    //Console.WriteLine(output);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            else
+            {
+                if (wifi.ConnectionStatus == WifiStatus.Connected)
+                    wifi.Disconnect();
+            }                    
         }
 
         public bool WifiConnect(Camera? camera)
@@ -43,41 +81,75 @@ namespace CameraExperiments
 
             Console.WriteLine("Connecting Wifi Network: " + ssid);
 
-            IEnumerable<AccessPoint> accessPoints = wifi.GetAccessPoints();
-
-            foreach (AccessPoint ap in accessPoints)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                if (ap.Name == ssid)
+                try
                 {
-                    AuthRequest authRequest = new AuthRequest(ap) { Password = password };
-
-                    if (!ap.IsConnected)
+                    ProcessStartInfo startInfo = new ProcessStartInfo()
                     {
-                        if (authRequest.IsPasswordRequired)
-                        {
-                            if (ap.HasProfile)
-                            {
-                                success = ap.Connect(authRequest);
-                            }
-                            else
-                            {
-                                try
-                                {
-                                    success = ap.Connect(authRequest, false);
-                                }
-                                catch (Win32Exception e)
-                                {
-                                    success = false;
-                                }
-                            }
+                        FileName = "nmcli",
+                        Arguments = "d wifi connect " + ssid + " password " + password,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    };
+                    
+                    Process proc = new Process() { StartInfo = startInfo };
+                    proc.Start();
 
-                            if (success)
+                    string output = proc.StandardOutput.ReadToEnd();
+                    proc.WaitForExit();
+
+                    //Console.WriteLine(output);
+
+                    if(output.ToLower().Contains("successfully activated"))
+                    {
+                        success = true;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            else
+            {
+                IEnumerable<AccessPoint> accessPoints = wifi.GetAccessPoints();
+
+                foreach (AccessPoint ap in accessPoints)
+                {
+                    if (ap.Name == ssid)
+                    {
+                        AuthRequest authRequest = new AuthRequest(ap) { Password = password };
+
+                        if (!ap.IsConnected)
+                        {
+                            if (authRequest.IsPasswordRequired)
                             {
-                                Console.WriteLine("Wifi connected - " + ssid);
+                                if (ap.HasProfile)
+                                {
+                                    success = ap.Connect(authRequest);
+                                }
+                                else
+                                {
+                                    try
+                                    {
+                                        success = ap.Connect(authRequest, false);
+                                    }
+                                    catch (Win32Exception e)
+                                    {
+                                        success = false;
+                                    }
+                                }
                             }
                         }
                     }
                 }
+            }
+
+            if (success)
+            {
+                Console.WriteLine("Wifi connected - " + ((camera == null)? "Primary / Local Wifi" : ssid));
             }
 
             if (success && sleepOnCompletion > 0)
