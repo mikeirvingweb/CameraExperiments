5e7043a2beb99a8d29917689ac02604ca4bd7582
Mike Irving <c-mirving@kahua.com>
2023-01-11 12:27:31 +0000

    perf: linux wifi improvements

--- CameraExperiments/Classes/Wifi.cs
+++ CameraExperiments/Classes/Wifi.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Text.Json;
 
 namespace CameraExperiments
@@ -24,32 +25,81 @@ namespace CameraExperiments
 
         public void WifiDisconnect()
         {
-            Console.WriteLine("Disconnecting from current Wifi Network.");
+            Console.WriteLine("Disconnecting from current Wifi Network.\n");
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 try
                 {
-                    ProcessStartInfo startInfo = new ProcessStartInfo()
+                    StringBuilder output = new StringBuilder();
+                    StringBuilder error = new StringBuilder();
+
+                    using (Process process = new Process())
                     {
-                        FileName = "nmcli",
-                        Arguments = "d disconnect wlan0",
-                        RedirectStandardOutput = true,
-                        CreateNoWindow = true,
-                        UseShellExecute = false
-                    };
+                        var timeout = 15000;
 
-                    Process proc = new Process() { StartInfo = startInfo };
-                    proc.Start();
+                        ProcessStartInfo startInfo = new ProcessStartInfo()
+                        {
+                            FileName = "nmcli",
+                            Arguments = "d disconnect wlan0",
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                            CreateNoWindow = true,
+                            UseShellExecute = false,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
 
-                    string output = proc.StandardOutput.ReadToEnd();
-                    proc.WaitForExit(15000);
+                        process.StartInfo = startInfo;
+
+                        using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+                        using (AutoResetEvent errorWaitHandle = new AutoResetEvent(false))
+                        {
+                            process.OutputDataReceived += (sender, e) => {
+                                if (e.Data == null)
+                                {
+                                    outputWaitHandle.Set();
+                                }
+                                else
+                                {
+                                    output.AppendLine(e.Data);
+                                }
+                            };
+                            process.ErrorDataReceived += (sender, e) =>
+                            {
+                                if (e.Data == null)
+                                {
+                                    errorWaitHandle.Set();
+                                }
+                                else
+                                {
+                                    error.AppendLine(e.Data);
+                                }
+                            };
+
+                            process.Start();
+
+                            process.BeginOutputReadLine();
+                            process.BeginErrorReadLine();
+
+                            if (process.WaitForExit(timeout) &&
+                                outputWaitHandle.WaitOne(timeout) &&
+                                errorWaitHandle.WaitOne(timeout))
+                            {
+                                // Process completed. Check process.ExitCode here.
+                            }
+                            else
+                            {
+                                // Timed out.
+                            }
+                        }
+                    }
 
                     //Console.WriteLine(output);
+                    //Console.WriteLine(error);
                 }
                 catch (Exception e)
                 {
-
+                    Console.WriteLine(e.Message);
                 }
             }
             else
@@ -79,38 +129,86 @@ namespace CameraExperiments
                 password = camera.WifiPassword;
             }
 
-            Console.WriteLine("Connecting Wifi Network: " + ssid.Substring(0, 3) + "*...");
+            Console.WriteLine("Connecting Wifi Network: " + ((camera == null) ? "Primary / Local Wifi" : ssid.Substring(0, 3) + "*..."));
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 try
                 {
-                    ProcessStartInfo startInfo = new ProcessStartInfo()
-                    {
-                        FileName = "Scripts/WifiLinux.sh",
-                        Arguments = ssid + " " + password,
-                        RedirectStandardOutput = true,
-                        CreateNoWindow = true,
-                        UseShellExecute = false,
-                        WindowStyle = ProcessWindowStyle.Hidden
-                    };
-                    
-                    Process proc = new Process() { StartInfo = startInfo };
-                    proc.Start();
+                    StringBuilder output = new StringBuilder();
+                    StringBuilder error = new StringBuilder();
 
-                    string output = proc.StandardOutput.ReadToEnd();
-                    proc.WaitForExit(15000);
+                    using (Process process = new Process())
+                    {
+                        var timeout = 15000;
+
+                        ProcessStartInfo startInfo = new ProcessStartInfo()
+                        {
+                            FileName = "Scripts/WifiLinux.sh",
+                            Arguments = ssid + " " + password,
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                            CreateNoWindow = true,
+                            UseShellExecute = false,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+
+                        process.StartInfo = startInfo;
+
+                        using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+                        using (AutoResetEvent errorWaitHandle = new AutoResetEvent(false))
+                        {
+                            process.OutputDataReceived += (sender, e) => {
+                                if (e.Data == null)
+                                {
+                                    outputWaitHandle.Set();
+                                }
+                                else
+                                {
+                                    output.AppendLine(e.Data);
+                                }
+                            };
+                            process.ErrorDataReceived += (sender, e) =>
+                            {
+                                if (e.Data == null)
+                                {
+                                    errorWaitHandle.Set();
+                                }
+                                else
+                                {
+                                    error.AppendLine(e.Data);
+                                }
+                            };
+
+                            process.Start();
+
+                            process.BeginOutputReadLine();
+                            process.BeginErrorReadLine();
+
+                            if (process.WaitForExit(timeout) &&
+                                outputWaitHandle.WaitOne(timeout) &&
+                                errorWaitHandle.WaitOne(timeout))
+                            {
+                                // Process completed. Check process.ExitCode here.
+                            }
+                            else
+                            {
+                                // Timed out.
+                            }
+                        }
+                    }
 
                     //Console.WriteLine(output);
+                    //Console.WriteLine(error);
 
-                    if (output.ToLower().Contains("successfully activated"))
+                    if (output.ToString().ToLower().Contains("successfully activated"))
                     {
                         success = true;
                     }
                 }
                 catch (Exception e)
                 {
-
+                    Console.WriteLine(e.Message);
                 }
             }
             else
@@ -148,22 +246,22 @@ namespace CameraExperiments
                 }
             }
 
+            Console.WriteLine("Wifi Connection " + (success ? "Successful." : "Unsuccessful.\n"));
+
             if (success)
             {
-                Console.WriteLine("Wifi connected - " + ((camera == null)? "Primary / Local Wifi" : ssid.Substring(0, 3) + "*..."));
-            }
-
-            if (success && sleepOnCompletion > 0)
-            {
-                Console.Write("Pause after Wifi Connection");
-
-                for (var i = 0; i < sleepOnCompletion; i++)
+                if (sleepOnCompletion > 0)
                 {
-                    Thread.Sleep(1000);
-                    Console.Write(".");
-                }
+                    Console.Write("Pause after Wifi Connection");
 
-                Console.WriteLine(Environment.NewLine);
+                    for (var i = 0; i < sleepOnCompletion; i++)
+                    {
+                        Thread.Sleep(1000);
+                        Console.Write(".");
+                    }
+
+                    Console.WriteLine(Environment.NewLine);
+                }
             }
 
             return success;
